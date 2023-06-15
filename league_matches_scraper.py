4fd554fc6e2ac54c7a079b199ef26a1bb0d931e7
Manuel Porras <60162615+Porras53@users.noreply.github.com>
2023-06-15 04:43:43 -0500

    Muchos cambios

--- league_matches_scraper.py
+++ league_matches_scraper.py
@@ -1,4 +1,6 @@
 # Imports
+import os
+import pandas as pd
 from utils import *
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,7 +11,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def league_matches_scraper(link, output_file, cant_omitir):
-    # Comentar la siguiente linea si solo se quiere agregar datos (esta linea crea un archivo nuevo)
+    file_exists = os.path.exists(output_file)
+    previous_data = {}
+    first_value = {}
+    if file_exists:
+        previous_data = pd.read_csv(output_file.split(".")[0] + ".csv", sep=',', encoding='utf-8', na_values='-')
+        first_value = previous_data.iloc[0]
+
+    # Se crea un archivo nuevo
     create_file(output_file)
 
     # Configuracion del WebDriver
@@ -34,10 +43,14 @@ def league_matches_scraper(link, output_file, cant_omitir):
     # Matches
     total = len(ids)
     completados = 0
+    terminar = False
+    index = 0
 
     total -= cant_omitir
-    # for i in ids[:-cant_omitir]:  # Temporada corta (ej: Colombia)
-    for i in ids:  # Temporada larga
+    # while (index < len(ids[:-cant_omitir])) and (not terminar):  # Temporada corta (ej: Colombia)
+    while (index < len(ids)) and (not terminar):  # Temporada larga
+        i = ids[index]
+
         # Match info retrieving
         try:
             time.sleep(0.25)
@@ -51,6 +64,7 @@ def league_matches_scraper(link, output_file, cant_omitir):
             print("Partido " + str(completados) + " de " + str(total) + ":")
             print("No se disputó")
             print()
+            index += 1
             continue
 
         # Header
@@ -68,11 +82,11 @@ def league_matches_scraper(link, output_file, cant_omitir):
         info_header.append(
             lines_header[7].split(">")[-1] + " " + lines_header[8].split(">")[-1])  # Home Team Name and Goals
         info_header.append(
-            lines_header[16].split(">")[-1] + " " + lines_header[10].split(">")[-1])  # Away Team Name and Goals
+            lines_header[17].split(">")[-1] + " " + lines_header[10].split(">")[-1])  # Away Team Name and Goals
 
         # Stats
         stats = str(soup).split("subTabs tabs__detail--sub")[1]
-        stats = stats.split("Cuotas prepartido")[0]
+        stats = stats.split("section__title")[0]
         temp = stats.split("</")
         lines_stats = []
         for j in temp:
@@ -81,7 +95,8 @@ def league_matches_scraper(link, output_file, cant_omitir):
 
         info_stats = []
         should_skip = True
-        for j in range(3, len(lines_stats) - 5, 5):
+        first_line = 11 if lines_stats[6].__contains__("xG") else 3
+        for j in range(first_line, len(lines_stats) - 5, 5):
             if lines_stats[j + 1].split(">")[-1] in nom_estadisticas:
                 info_stats.append(
                     [lines_stats[j].split(">")[-1], lines_stats[j + 1].split(">")[-1],
@@ -93,6 +108,7 @@ def league_matches_scraper(link, output_file, cant_omitir):
             print("Partido " + str(completados) + " de " + str(total) + ":")
             print("No se disputó")
             print()
+            index += 1
             continue
 
         # Escritura del archivo excel
@@ -123,10 +139,10 @@ def league_matches_scraper(link, output_file, cant_omitir):
         stats_gen.append(info_header[1])
         datos_local = info_header[2]
         datos_visita = info_header[3]
-        stats_gen.append(datos_local[:-2])
-        stats_gen.append(datos_visita[:-2])
-        stats_gen.append(datos_local[-2:])
-        stats_gen.append(datos_visita[-2:])
+        stats_gen.append(datos_local[:-2].strip())
+        stats_gen.append(datos_visita[:-2].strip())
+        stats_gen.append(datos_local[-2:].strip())
+        stats_gen.append(datos_visita[-2:].strip())
 
         contador = 0
         for j in info_stats:
@@ -147,16 +163,29 @@ def league_matches_scraper(link, output_file, cant_omitir):
 
         stats_gen.append(int(datos_local[-2:]) - int(datos_visita[-2:]))  # Resultado
 
-        hoja.append(stats_gen)
-        wb.save(output_file)
+        if file_exists and (first_value["Date"] == stats_gen[0]) and (
+                first_value["HomeTeam"] == stats_gen[1]) and (
+                first_value["AwayTeam"] == stats_gen[2]):
+            print("Datos anteriores encontrados. Actualizando archivo")
+            print()
+            for i in range(len(previous_data)):
+                hoja.append(previous_data.iloc[i].tolist())
+                wb.save(output_file)
+            terminar = True
 
-        # Console printing
-        completados += 1
-        print("Partido " + str(completados) + " de " + str(total) + ":")
-        print(datos_local[:-2] + " vs " + datos_visita[:-2])
-        print(datos_local[-1:] + " a " + datos_visita[-1:])
-        print(info_stats)
-        print()
+        if not terminar:
+            hoja.append(stats_gen)
+            wb.save(output_file)
+
+            # Console printing
+            completados += 1
+            print("Partido " + str(completados) + " de " + str(total) + ":")
+            print(datos_local[:-2] + " vs " + datos_visita[:-2])
+            print(datos_local[-1:] + " a " + datos_visita[-1:])
+            print(info_stats)
+            print()
+
+        index += 1
 
     # Elimina la hoja por defecto
     eliminar_hoja_por_defecto(output_file)
@@ -166,7 +195,6 @@ def league_matches_scraper(link, output_file, cant_omitir):
 
     # Convierte el archivo excel en un archivo CSV
     convertir_excel_a_csv(output_file)
-
 
 # "Main"
 # with open("./data/linksLigas.txt") as archivo:
--- main.py
+++ main.py
@@ -11,17 +11,15 @@ cant_omitir = 0
 link_liga = "https://www.flashscore.co/futbol/{}/{}/".format(pais, liga)
 
 # Lectura de versión
-file = open("versiones/{}_{}.txt".format(pais, liga), "r")
-version = int(file.read())
-file.close()
+version = 0
 
-file = open("versiones/{}_{}.txt".format(pais, liga), "w")
-file.write("{}\n".format(version+1))
-file.close()
+# Especificación de la ruta con los datos
+if not os.path.exists(f'data/{pais}_{liga}'):
+    os.mkdir(os.path.abspath(os.getcwd()).replace("\\", "/") + "/" + f'data/{pais}_{liga}')
 
-# Creación de carpeta de datos
 path = f'data/{pais}_{liga}/version{version}'
-os.mkdir(os.path.abspath(os.getcwd()).replace("\\", "/") + "/" + path)
+if not os.path.exists(path):
+    os.mkdir(os.path.abspath(os.getcwd()).replace("\\", "/") + "/" + path)
 
 # Ejecución del programa
 print("-------------------- Parte 1 de 3 --------------------")
@@ -29,12 +27,12 @@ print("Recolectando estadísticas anteriores")
 print()
 league_matches_scraper(link_liga + "resultados/", path + "/resultados_anteriores.xlsx", cant_omitir)
 
-print("-------------------- Parte 2 de 3 --------------------")
-print("Calculando estadísticas promedio")
-print()
-average_stats_calculator(path + "/resultados_anteriores.xlsx")
+# print("-------------------- Parte 2 de 3 --------------------")
+# print("Calculando estadísticas promedio")
+# print()
+# average_stats_calculator(path + "/resultados_anteriores.xlsx")
 
-print("-------------------- Parte 3 de 3 --------------------")
-print("Obteniendo lista de próximos partidos")
-print()
-upcoming_matches_scraper(link_liga + "partidos/", path + "/proximos_partidos.xlsx")
+# print("-------------------- Parte 3 de 3 --------------------")
+# print("Obteniendo lista de próximos partidos")
+# print()
+# upcoming_matches_scraper(link_liga + "partidos/", path + "/proximos_partidos.xlsx")
